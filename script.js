2e9192903bc55169f9f929219ced6c3414093908
sunil kumar <webdevlopersunil1997@gmail.com>
2023-12-18 13:07:19 +0530

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+function Togglebar(){
+    var a = document.getElementById("toggle");
+
+    if(a.style.display ==="block")
+    {
+        a.style.display="none"
+    }
+    else{
+        a.style.display="block"
+    }
+}
